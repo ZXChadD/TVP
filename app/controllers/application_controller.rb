762aab8e1469feb6d18065c2aacbffdd459a0d55
ZXChadd <zhexianchadd@gmail.com>
2017-11-14 12:01:17 +0800

    Edit providers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname address tel_num experience description license_num avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :address, :tel_num, :experience, :description, :license_num, :avatar, :company, :industry, provider_attaches_attributes: %i[id image title provider_id]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :address, :tel_num, :experience, :description, :license_num, :avatar, :company, :industry, :experience_consulting, :daily, :previous, provider_attaches_attributes: %i[id image title provider_id]])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113163605) do
+ActiveRecord::Schema.define(version: 20171114024709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,10 @@ ActiveRecord::Schema.define(version: 20171113163605) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "experience_consulting"
+    t.string "previous"
+    t.string "daily"
+    t.string "industry"
     t.index ["email"], name: "index_providers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_providers_on_reset_password_token", unique: true
   end
